20bf99226d9262c551fdf3fabca87a0e4c1604f7
清净 <qingjing666666@126.com>
2026-02-23 20:32:38 +0800

    deploy

--- reading-ui/src/views/index/HomePage.vue
+++ reading-ui/src/views/index/HomePage.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div>HomePage</div>
-	<div>HomePage</div>
-	<div>HomePage</div>
-	<div>HomePage</div>
-	<div>HomePage</div>
-	<div>HomePage</div>
-	<div>HomePage</div>
-	<div>HomePage</div>
-	<div>HomePage</div>
-	<div>HomePage</div>
+    <div>HomePage</div>
 </template>
 
 <style scoped></style>
